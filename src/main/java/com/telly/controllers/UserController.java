947aeebbfd47e9da9a2925ffab0cef293bc110af
Zack-19 <jz_safa@esi.dz>
2023-01-08 22:31:27 +0000

    Création

--- src/main/java/com/telly/controllers/UserController.java
+++ src/main/java/com/telly/controllers/UserController.java
@@ -39,6 +39,32 @@ public class UserController {
 	}
 
 
+	@RequestMapping("/createaccount")
+	public String createAccount(Model model, Principal principal) {
+
+		model.addAttribute("user", new User());
+
+		return "createaccount";
+	}
+
+	@RequestMapping(value = "/createuser", method = RequestMethod.POST)
+	public String createUser(@Validated(FormValidationGroup.class) User user, BindingResult result) {
+
+		if(result.hasErrors()) {
+			return "createaccount";
+		}
+
+		user.setAuthority("ROLE_USER");
+		user.setEnabled(true);
+
+		userService.create(user);
+
+		return "home";
+
+	}
+
+
+
 }
 
 
